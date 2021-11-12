d9ca4c5744aa34dadf3f893e4b8ea0a7d2738a52
bachbo03 <tanbach2003@gmail.com>
2021-11-12 10:53:30 -0500

    Update f string. py.py

--- f string. py.py
+++ f string. py.py
@@ -1,18 +1,2 @@
-def formatPhone(n1, n2, n3):
-    #formatted = "(" + n1 + ') ' + n2 + '-' + n3
-    formatted = f"({n1}) {n2}-{n3}"
-    return formatted
-print(formatPhone("647", "685", "9456"))
-
-def multiplier(n1, n2):
-    product = int(n1) * int(n2)
-    return f"multiple of {n1} and {n2} is {product}"
-print(multiplier(5,5))
-
-def moneyformat(n1):
-    return f"${n1:.2f}"
-
-print(moneyformat(29.13847))
-
-def mon_vert(n1):
-    return 
+for f in [1, 2, 3 ,4]:
+    print(f)
